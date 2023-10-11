3b8cd351a7fc00171b92cc2f6fb35f2bebcce5d5
Daniela Jureckova <dannyjurda@gmail.com>
2023-10-11 20:50:17 +0200

    indent 4

--- 07_routes.py
+++ 07_routes.py
@@ -5,10 +5,12 @@ import pprint
 import json
 import geopandas
 
-coords = ((16.61304474,49.19127175),(16.59422636,49.21028027))
+coords = ((16.61304474, 49.19127175), (16.59422636, 49.21028027))
 
-client = openrouteservice.Client(key='5b3ce3597851110001cf6248dc769df6350b4634869b55b7f7ef7e03') # Specify your personal API key
-routes = directions(client,coordinates=coords, profile='cycling-regular')
+# Specify your personal API key
+client = openrouteservice.Client(
+    key='5b3ce3597851110001cf6248dc769df6350b4634869b55b7f7ef7e03')
+routes = directions(client, coordinates=coords, profile='cycling-regular')
 geometry = routes['routes'][0]['geometry']
 decoded = convert.decode_polyline(geometry)
 
@@ -17,4 +19,4 @@ pprint.pprint(routes)
 print(decoded)
 
 with open("07_routes_01_nextbike.json", mode="w", encoding="utf-8") as outputfile:
-    json.dump(decoded, outputfile)
+    json.dump(decoded, outputfile, indent=4)
