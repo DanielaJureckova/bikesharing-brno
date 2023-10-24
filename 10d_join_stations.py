6f0480b6e734a9ba00176cd837eb931f73c4fd22
Iva Benešová <iva.beenesova@gmail.com>
2023-10-24 18:14:17 +0200

    edit for data agregation

--- 10d_join_stations.py
+++ 10d_join_stations.py
@@ -6,12 +6,12 @@ import csv
 from geopy.geocoders import Nominatim
 from geopy.distance import geodesic
 
-# calculates distance between two places, where place = (lat, lgn)
+#calculates distance between two places, where place = (lat, lgn)
 def get_distance(place1, place2):
     distance = geodesic(place1, place2).kilometers  
     return distance
 
-rekola = pd.read_csv("rekola_gps_final.csv")
+rekola = pd.read_csv("rekola_stations_final.csv")
 
 nextbike = pd.read_csv("nextbike_stations_with_adr.csv")
 nextbike = nextbike.rename(columns={"lng": "lgn"})
@@ -20,8 +20,6 @@ nextbike_bikes = pd.read_csv("nxbike_bikes_stations.csv")
 
 joined = pd.concat([nextbike, nextbike_bikes])
 
-print(joined)
-
 station_list = []
 
 for index, row in joined.iterrows():
@@ -29,8 +27,7 @@ for index, row in joined.iterrows():
     lat = row["lat"]
     lgn = row["lgn"]
     count = row["count"]
-    place = row["place"]
-    station_list.append([address, lat, lgn, count, place])
+    station_list.append([address, lat, lgn, count])
 
 index = 0
 count = 0
@@ -42,23 +39,23 @@ for place1 in station_list:
     
     for place2 in station_list:
         coordinates2 = (place2[1],place2[2])
-        if get_distance(coordinates1, coordinates2) < 0.03:
-            station_list2[index] = [place1[0], place1[1], place1[2], place2[3], place1[4]]
+        if get_distance(coordinates1, coordinates2) < 0.06:
+            station_list2[index] = [place1[0], place1[1], place1[2], place2[3]]
         index += 1
 
     index = 0 
 
 
-stations_df = pd.DataFrame(station_list2, columns = ['address', 'lat', "lgn", "count", "place"])
-stations_final = stations_df.groupby(['address', 'lat', "lgn"])["count"].sum()
+stations_df = pd.DataFrame(station_list2, columns = ['address', 'lat', "lgn", "count"])
+stations_final = stations_df.groupby(['address']).agg({'count': 'sum', 'lat': 'median',"lgn":"median"})
 stations_final_df = stations_final.reset_index()
 stations_final_df.sort_values(by = ["count"], ascending=False, inplace = True)
 
-stations_final_places = pd.merge(stations_final_df, nextbike, on = ["address", "lat", "lgn", "count"], how="left" ) 
+
+stations_final_df.to_csv("stations_all2.csv", index = False)
 
 
 
-stations_final_places.to_csv("stations_all.csv")
 
 
 
