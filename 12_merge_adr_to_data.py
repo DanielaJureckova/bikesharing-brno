e5da78ddb39ec1505484de64b6c84899c5fbade8
Iva Benešová <iva.beenesova@gmail.com>
2023-11-01 19:23:12 +0100

    use geopandas

--- 12_merge_adr_to_data.py
+++ 12_merge_adr_to_data.py
@@ -1,61 +1,59 @@
-import geopy        
+import geopandas as gpd
 import pandas as pd
-import csv
-import json
-from geopy.geocoders import Nominatim
-from geopy.distance import geodesic
+import numpy as np
 
+from scipy.spatial import cKDTree
+from shapely.geometry import Point
 
-# addition of the closest start/end station (address) to each row, exact gps
-# v datech manuální edit na řádku 153307: upravena lat z 49183.0 na 49.183
+def ckdnearest(gdA, gdB):
 
-def get_distance(place1, place2):
-    distance = geodesic(place1, place2).kilometers  
-    return distance
+    nA = np.array(list(gdA.geometry.apply(lambda x: (x.x, x.y))))
+    nB = np.array(list(gdB.geometry.apply(lambda x: (x.x, x.y))))
+    btree = cKDTree(nB)
+    dist, idx = btree.query(nA, k=1)
+    gdB_nearest = gdB.iloc[idx].drop(columns="geometry").reset_index(drop=True)
+    gdf = pd.concat(
+        [
+            gdA.reset_index(drop=True),
+            gdB_nearest,
+            pd.Series(dist, name='dist')
+        ], 
+        axis=1)
 
-def find_start_location(row):
-    min_distance = float('inf')
-    start_location = None
-    for station in stations:
-        distance = geodesic((row['start_latitude'], row['start_longitude']),
-                           (station['latitude'], station['longitude'])).kilometers
-        if distance < min_distance:
-            min_distance = distance
-            if min_distance < 0.05:
-                start_location = station['station_index']
-            else:
-                start_location = "N/A"
-                
-    return start_location
+    return gdf
 
-def find_end_location(row):
-    min_distance = float('inf')
-    end_location = None
-    for station in stations:
-        distance = geodesic((row['end_latitude'], row['end_longitude']),
-                           (station['latitude'], station['longitude'])).kilometers
-        if distance < min_distance:
-            min_distance = distance
-            if min_distance < 0.10:
-                end_location = station['station_index']
-            else:
-                end_location = "N/A"
-                
-    return end_location
+#převedení na geodataframes
+stations = pd.read_csv("stations_adr_str_el.csv")
+stations["index"] = stations.index
+stations['geometry'] = stations.apply(lambda row: Point(row.longitude, row.latitude), axis=1)
+stations_gdf = gpd.GeoDataFrame(stations, geometry='geometry')
 
-trips = pd.read_csv("next_rekola_both.csv")
+data = pd.read_csv("next_rekola_both.csv")
+stations['geometry'] = stations.apply(lambda row: Point(row.longitude, row.latitude), axis=1)
 
-stations_all = pd.read_csv("10e_stations_ad_el.csv")
-stations_all['station_index'] = stations_all.index
+data['geometry'] = data.apply(lambda row: Point(row.start_longitude, row.start_latitude), axis=1)
+data_gdf = gpd.GeoDataFrame(data, geometry='geometry')
 
-stations = stations_all.to_dict(orient='records')
+result_start_gdf = ckdnearest(data_gdf, stations_gdf)
+result_start_gdf = result_start_gdf.rename(columns={'street': 'start_street', "address":"start_address", "elevation":"start_elevation", "index": "start_location"})
+result_start_gdf.drop(columns=['dist', 'count', "latitude", "longitude"], inplace = True)
 
-# Přiřazení nejbližšího indexu lokality k GPS souřadnicím
-trips['start_location'] = trips.apply(find_start_location, axis=1)
-trips['end_location'] = trips.apply(find_end_location, axis=1)
+result_start = result_start_gdf.drop(columns='geometry')
 
+result_start['geometry2'] = result_start.apply(lambda row: Point(row.end_longitude, row.end_latitude), axis=1)
+end_gdf = gpd.GeoDataFrame(result_start, geometry='geometry2')
 
-trips.to_csv("23-10-25_data_merged_adr.csv", index = False)
+result_end_gdf = ckdnearest(end_gdf, stations_gdf)
+result_end_gdf = result_end_gdf.rename(columns={'street': 'end_street', "address":"end_address", "elevation":"end_elevation", "index": "end_location"})
+result_end_gdf.drop(columns=['dist', 'count','latitude', 'longitude'], inplace = True)
+
+result_end = result_end_gdf.drop(columns='geometry2')
+
+result_end = result_end[['start_time', 'end_time', 'duration', 'user_id', 'start_latitude',
+       'start_longitude', 'end_latitude', 'end_longitude', 'company', 'start_location',
+       'start_address', 'start_street', 'start_elevation', 'end_location',
+       'end_address','end_street', 'end_elevation']]
+result_end.to_csv("output.csv", index = False)
 
 
 
