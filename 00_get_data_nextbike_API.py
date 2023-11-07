1d15325032feb445ade4aa046097b3c6b0f31739
Iva Benešová <iva.beenesova@gmail.com>
2023-11-07 10:09:25 +0100

    rename file

---
+++ 00_get_data_nextbike_API.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import time
+import json
+import requests
+
+url = 'https://api.nextbike.net/maps/nextbike-live.json?city=660'
+response = requests.get(url).json()
+timestr = time.strftime("%Y%m%d-%H%M%S")
+filename = 'json/response'+timestr+'.json'
+with open(filename, 'w') as outfile:
+    json.dump(response, outfile)
+
