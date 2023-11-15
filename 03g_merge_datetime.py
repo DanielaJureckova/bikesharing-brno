1b6f835f2e866875f4ec6315e259b920d3974905
Daniela Jureckova <dannyjurda@gmail.com>
2023-11-15 13:16:08 +0100

    adding address and addresid to dataset

--- 03g_merge_datetime.py
+++ 03g_merge_datetime.py
@@ -1,5 +1,5 @@
 import pandas as pd
-
+import numpy as np
 df = pd.read_csv("Data/streamlit/23-11-13_17-50_data.csv")
 
 day_name_cz = {
@@ -21,6 +21,33 @@ df['month_name_cz'] = df['month'].map(month_name_cz)
 
 df["month_year"] = df["start_time"].dt.strftime('%m/%y')
 
-df.drop(columns=['start_address','end_address'], inplace=True)
+unique_addresses = df['start_address'].unique()
 
-df.to_csv("./Data/merged/23-11-13_21-45_no_address.csv", index=False)
+unique_addresses = np.append(unique_addresses, df['end_address'].unique())
+
+unique_addresses = set(unique_addresses)
+
+unique_addresses = list(unique_addresses)
+
+df_address = pd.DataFrame(unique_addresses)
+
+df_address.index.name = "ID"
+df_address.columns = ['address']
+
+df_address = df_address.assign(row_number=range(len(df_address)))
+
+print("-----------------")
+
+df = df.merge(df_address, how='left', left_on='start_address', right_on='address')
+print(df.columns)
+df = df.rename(columns={"row_number": "start_address_id",})
+
+df = df.merge(df_address, how='left', left_on='end_address', right_on='address')
+
+df = df.rename(columns={"row_number": "end_address_id",})
+
+df_address.to_csv("./Data/merged/address.csv", index=True)
+
+df.drop(columns=['start_address','end_address','address_y','address_x'], inplace=True)
+
+df.to_csv("./Data/merged/23-11-14_20-40_address_id.csv", index=False)
